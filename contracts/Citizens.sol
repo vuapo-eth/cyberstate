804402a2d9ce076e55410e9a5366026ffda311b0
Lukas Tassanyi <microhash@twitter.com>
2022-10-05 22:18:38 -0300

    create Citizens.sol

---
+++ contracts/Citizens.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Citizens {
+    mapping (address => bool) public is_citizen;
+    mapping (uint256 => address) public address_by_id;
+    mapping (address => uint256) public id_by_address;
+    uint256 public population = 0;
+
+    function register_citizen(address address_of_citizen) public {
+        require(!is_citizen[address_of_citizen], "citizen already exists");
+        population = population + 1;
+        uint256 id = population;
+        is_citizen[address_of_citizen] = true;
+        address_by_id[id] = address_of_citizen;
+        id_by_address[address_of_citizen] = id;
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('custom-env').env()
+
+const CONTRACT_ADDRESS = "0x55e39a7f9Ab12CCADe517076dbcDD547c582AB61";
+
+task("deploy", "Deploys the contract", async (taskArgs, hre) => {
+    const Citizens = await hre.ethers.getContractFactory("Citizens");
+    const citizens = await Citizens.deploy();
+    await citizens.deployed();
+    console.log("Citizens.sol deployed to: " + citizens.address);
+});
+
+task("register_citizen", "Registers a citizen")
+    .addParam("address", "The EVM address of the new citizen")
+    .setAction(async (taskArgs, hre) => {
+        const Citizens = await hre.ethers.getContractFactory("Citizens");
+        const citizens = await Citizens.attach(CONTRACT_ADDRESS);
+        await citizens.register_citizen(taskArgs.address);
+        let id = await citizens.id_by_address(taskArgs.address);
+        console.log("Citizen #"+id+" ('" + taskArgs.address + "') added");
+    });
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+    solidity: "0.8.17",
+    defaultNetwork: "shimmer_testnet",
+    networks: {
+        shimmer_testnet: {
+            url: "https://api.sc.testnet.shimmer.network/chain/rms1prr4r7az8e46qhagz5atugjm6x0xrg27d84677e3lurg0s6s76jr59dw4ls/evm/jsonrpc",
+            accounts: [process.env.PRIVATE_KEY]
+        }
+    }
 };
